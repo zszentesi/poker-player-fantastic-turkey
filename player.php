1e821b1d936dab36be24f9fec310ff5a0548cdcd
Zsolt Szentesi <zsolt.szentesi@logmein.com>
2018-10-25 11:40:13 +0200

    lol

--- player.php
+++ player.php
@@ -6,10 +6,33 @@ class Player
 
     public function betRequest($game_state)
     {
+        foreach ($game_state['players'] as $player) {
+            if(array_key_exists('hole_cards', $player)){
+                $me = $player;
+                $my_hand = $me['hole_cards'];
+
+                if($this->isItGoodHand($my_hand)) {
+                    return 1000;
+                }
+            }
+        }
         return 0;
     }
 
     public function showdown($game_state)
     {
+    }
+
+    public function isItGoodHand($hand){
+        $card1 = $hand[0];
+        $card2 = $hand[1];
+
+        if($card1['suit'] == $card2['suit'] ){
+            return true;
+        }
+        if($card1['rank'] == $card2['rank'] ){
+            return true;
+        }
+        return false;
     }
 }
