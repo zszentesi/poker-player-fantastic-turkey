2e9f10c415cf0cf69c3ab60a3dc97061a4323d75
Zsolt Szentesi <zsolt.szentesi@logmein.com>
2018-10-25 13:03:24 +0200

    shark v.05

--- index.php
+++ index.php
@@ -3,14 +3,17 @@
 require_once('player.php');
 
 $state = json_decode($_POST['game_state'], true);
-$player = new Player($state);
+
+
 
 switch($_POST['action'])
 {
     case 'bet_request':
+        $player = new Player($state);
         echo $player->betRequest();
         break;
     case 'showdown':
+        $player = new Player($state);
         $player->showdown();
         break;
     case 'version':
